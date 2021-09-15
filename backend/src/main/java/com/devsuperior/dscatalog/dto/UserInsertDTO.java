60414a199cfe3f63f3377f73a8e6ebf2fb7f796d
rqguzman <rafael.guzman@outlook.com>
2021-09-15 18:57:06 -0300

    :sparkles: Feature(User): Add endpoints

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.dto;
+
+public class UserInsertDTO extends UserDTO {
+
+    private static final long serialVersionUID = 1L;
+
+    private String password;
+
+    UserInsertDTO() {
+        super();
+    }
+
+    public UserInsertDTO(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
