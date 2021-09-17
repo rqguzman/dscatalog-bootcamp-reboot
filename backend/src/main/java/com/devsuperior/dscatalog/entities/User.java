a76aadf638d7939034daa34b7d5743a6a1432bdd
rqguzman <rafael.guzman@outlook.com>
2021-09-17 12:37:21 -0300

    :recycle: Refactor(User): Include e-mail uniqueness

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -15,9 +15,14 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
+
     private String lastName;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
